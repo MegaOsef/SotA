3c299dccd8b00751b6ab319cb52cad685efd22a7
Megaosef <Mega@osef.net>
2026-06-08 16:36:57 +0200

    fixes boss kill detection not working in 3.3.5a

--- src/Modules/BossKillTracker.lua
+++ src/Modules/BossKillTracker.lua
@@ -3,24 +3,20 @@ local SOTA = SOTAG
 local module = SOTA:NewModule("BossKillTracker", "AceEvent-3.0")
 
 function module:OnEnable()
-	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH")
+	self:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 end
 
-function module:CHAT_MSG_COMBAT_HOSTILE_DEATH(_, msg)
-	-- Example boss kill message: "Onyxia dies." — handle when that's contained in a longer message
-	local lowerMsg = string.lower(msg)
-	local findStr = " dies."
-	local s, e = string.find(lowerMsg, findStr, 1, true)
-
-	if s then
-		local bossTriggerName = string.sub(msg, 1, s - 1)
+function module:COMBAT_LOG_EVENT_UNFILTERED( _, _, subevent, _, _, _, _, destName, ...)
+	if subevent == "PARTY_KILL" then
+		local bossTriggerName = destName
 		if bossTriggerName ~= "" then
 			local bossDkpListItem = SOTA:FindBossDkpFromTriggerName(bossTriggerName)
 			SOTA:Debug("BossKillTracker: Detected kill:", bossTriggerName, "SOTA Boss Name/DKP:",
-			bossDkpListItem and bossDkpListItem.bossName or "unknown",
+				bossDkpListItem and bossDkpListItem.bossName or "unknown",
 				bossDkpListItem and bossDkpListItem.dkpValue or "N/A")
 			if bossDkpListItem then
-				self:SendMessage("SOTA_BOSS_KILLED", tostring(bossDkpListItem.bossName), tonumber(bossDkpListItem.dkpValue))
+				self:SendMessage("SOTA_BOSS_KILLED", tostring(bossDkpListItem.bossName),
+					tonumber(bossDkpListItem.dkpValue))
 				return
 			end
 		end
